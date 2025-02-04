4905b1be54b91ac17295cae8cfbd1a0760e94d29
palumbcl <cpalumbo.clement@gmail.com>
2025-02-04 17:15:50 +0100

    bonus 11

--- tp-devops-correction-docker-main/simple-api/src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
+++ tp-devops-correction-docker-main/simple-api/src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
@@ -30,7 +30,7 @@ class DepartmentsServiceTest {
     @Test
     void testGetDepartmentByName() {
         when(departmentDAO.findDepartmentByName("DepartmentTest")).thenReturn(DEPARTMENT);
-        assertEquals(DEPARTMENT, departmentDAO.findDepartmentByName("DepartmentTest"));
+        assertEquals(null, departmentDAO.findDepartmentByName("DepartmentTest"));
     }
 
     @Test
